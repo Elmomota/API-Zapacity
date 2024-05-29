b03e6e3b4d27e687d1c65015e66342fd5e354dca
Elmomota <163007475+Elmomota@users.noreply.github.com>
2024-05-29 14:24:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors"); 
+const app = express();
+const port = 3002;
+
+const { getZapatillas, getZapatillasById } = require("./controllers/getZapatillas");
+
+// Permitir solicitudes CORS desde cualquier origen
+// app.use(cors());
+
+// Permitir solicitudes CORS solo desde http://127.0.0.1:5502
+app.use(cors());
+
+app.get("/", getZapatillas);
+
+app.get("/:id", getZapatillasById);
+
+app.listen(port, () => {
+    console.log(`Corriendo ok en el puerto : ${port}`);
+});
---
+++ zapatillas.js
@@ -0,0 +1,78 @@
+const zapatillas = [
+    {
+      "id": "001",
+      "nombre": "Zapatillas deportivas Nike Air Max",
+      "descripcion": "Zapatillas deportivas Nike Air Max con tecnología Air para una máxima comodidad.",
+      "precio": "150.00",
+      "foto":"https://i.postimg.cc/TYbKgwJW/Nike-Air-Max.jpg",
+      "disponible": true
+    },
+    {
+      "id": "002",
+      "nombre": "Zapatillas Adidas Ultraboost",
+      "descripcion": "Zapatillas Adidas Ultraboost con amortiguación Boost para una excelente energía de retorno.",
+      "precio": "170.00",
+      "foto": "https://i.postimg.cc/htrVwr3B/adidas-ultraboost.jpg",
+      "disponible": true
+    },
+    {
+      "id": "003",
+      "nombre": "Zapatillas Reebok Classic",
+      "descripcion": "Zapatillas Reebok Classic con estilo retro y comodidad duradera.",
+      "precio": "80.00",
+      "foto": "https://i.postimg.cc/66yzvDbG/Reebok-Classic.jpg",
+      "disponible": true
+    },
+    {
+      "id": "004",
+      "nombre": "Zapatillas Puma RS-X",
+      "descripcion": "Zapatillas Puma RS-X con diseño futurista y comodidad avanzada.",
+      "precio": "120.00",
+      "foto": "https://i.postimg.cc/DZKFXrnS/descargar.jpg",
+      "disponible": true
+    },
+    {
+      "id": "005",
+      "nombre": "Zapatillas New Balance 574",
+      "descripcion": "Zapatillas New Balance 574 con estilo clásico y comodidad superior.",
+      "precio": "100.00",
+      "foto": "https://i.postimg.cc/q7Ysp7cB/New-Balance-574.jpg",
+      "disponible": true
+    },
+    {
+      "id": "006",
+      "nombre": "Zapatillas Vans Old Skool",
+      "descripcion": "Zapatillas Vans Old Skool con diseño icónico y comodidad todo el día.",
+      "precio": "65.00",
+      "foto": "https://i.postimg.cc/N0yXCw33/Vans-Old-Skool.jpg",
+      "disponible": true
+    },
+    {
+      "id": "007",
+      "nombre": "Zapatillas Converse Chuck Taylor All Star",
+      "descripcion": "Zapatillas Converse Chuck Taylor All Star, un clásico atemporal.",
+      "precio": "55.00",
+      "foto": "https://i.postimg.cc/pVBmZT99/Converse-Chuck-Taylor-All-Star.jpg",
+      "disponible": true
+    },
+    {
+      "id": "008",
+      "nombre": "Zapatillas Under Armour HOVR Phantom",
+      "descripcion": "Zapatillas Under Armour HOVR Phantom con amortiguación HOVR para una sensación de gravedad cero.",
+      "precio": "140.00",
+      "foto": "https://i.postimg.cc/rFPFntqp/Under-Armour-HOVR-Phantom.jpg",
+      "disponible": true
+    },
+    {
+      "id": "009",
+      "nombre": "Zapatillas Asics Gel-Kayano",
+      "descripcion": "Zapatillas Asics Gel-Kayano con tecnología GEL para una amortiguación superior.",
+      "precio": "160.00",
+      "foto":"https://i.postimg.cc/6pZwMjHH/Asics-Gel-Kayano.jpg",
+      "disponible": true
+    }
+  ];
+
+module.exports = {
+    zapatillas
+}
